37db975486b76f020b8bde57d04438c0f5f69248
Yakup Adaklı <yakup.adakli@gmail.com>
2016-11-17 18:08:17 +0300

    Duplicate ccleaner view added.

--- duplicate_cleaner/urls.py
+++ duplicate_cleaner/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from duplicate_cleaner.views import IndexView, HomeView, PlayListView, PlayListDetailView
+from duplicate_cleaner.views import IndexView, HomeView, PlayListView, PlayListDetailView, DuplicateCleanerView
 from yspotify.decorators import anonymous_required
 
 urlpatterns = [
@@ -10,4 +10,6 @@ urlpatterns = [
     url(r'^home/playlist/$', login_required(PlayListView.as_view()), name="playlists"),
     url(r'^home/playlist/(?P<playlist_id>[^/]+)/$',
         login_required(PlayListDetailView.as_view()), name="playlist-detail"),
+    url(r'^clean/duplicate/(?P<playlist_id>[^/]+)/$',
+        login_required(DuplicateCleanerView.as_view()), name="clean-duplicate"),
 ]
--- duplicate_cleaner/views.py
+++ duplicate_cleaner/views.py
@@ -2,8 +2,9 @@ import spotipy
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
+from django.utils.translation import ugettext
 
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 
 
 class IndexView(TemplateView):
@@ -20,8 +21,28 @@ class SpotifyMixin(object):
         try:
             return super(SpotifyMixin, self).get(request, **kwargs)
         except spotipy.SpotifyException, e:
-            messages.warning(request, e)
+            messages.warning(request, ugettext(u"The Access Token expired."))
             return redirect("%s?next=/" % reverse("auth:logout"))
+
+    def get_tracks(self, request, playlist_id, offset=0):
+        social_auth = request.user.social_auth.first()
+        sp = spotipy.Spotify(auth=social_auth.access_token)
+        data = sp.user_playlist_tracks(request.user.username, playlist_id, offset=offset)
+        items = data.get("items")
+        if data.get("next"):
+            offset = data.get("offset") + data.get("limit")
+            return self.get_tracks(playlist_id, offset=offset) + items
+        return items
+
+    def get_playlists(self, request, offset=0):
+        social_auth = request.user.social_auth.first()
+        sp = spotipy.Spotify(auth=social_auth.access_token)
+        data = sp.user_playlists(request.user.username, offset=offset)
+        items = data.get("items")
+        if data.get("next"):
+            offset = data.get("offset") + data.get("limit")
+            return self.get_playlists(offset=offset) + items
+        return items
 
 
 class PlayListView(SpotifyMixin, TemplateView):
@@ -29,18 +50,8 @@ class PlayListView(SpotifyMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(PlayListView, self).get_context_data(**kwargs)
-        context["playlists"] = self.get_playlists()
+        context["playlists"] = self.get_playlists(self.request)
         return context
-
-    def get_playlists(self, offset=0):
-        social_auth = self.request.user.social_auth.first()
-        sp = spotipy.Spotify(auth=social_auth.access_token)
-        data = sp.user_playlists(self.request.user.username, limit=2, offset=offset)
-        items = data.get("items")
-        if data.get("next"):
-            offset = data.get("offset") + data.get("limit")
-            return self.get_playlists(offset=offset) + items
-        return items
 
 
 class PlayListDetailView(SpotifyMixin, TemplateView):
@@ -49,15 +60,27 @@ class PlayListDetailView(SpotifyMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super(PlayListDetailView, self).get_context_data(**kwargs)
         playlist_id = kwargs.get("playlist_id")
-        context["tracks"] = self.get_tracks(playlist_id)
+        context["tracks"] = self.get_tracks(self.request, playlist_id)
         return context
 
-    def get_tracks(self, playlist_id, offset=0):
-        social_auth = self.request.user.social_auth.first()
-        sp = spotipy.Spotify(auth=social_auth.access_token)
-        data = sp.user_playlist_tracks(self.request.user.username, playlist_id, limit=2, offset=offset)
-        items = data.get("items")
-        if data.get("next"):
-            offset = data.get("offset") + data.get("limit")
-            return self.get_tracks(playlist_id, offset=offset) + items
-        return items
+
+class DuplicateCleanerView(SpotifyMixin, RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("playlist-detail", kwargs={"playlist_id": kwargs.get("playlist_id")})
+
+    def get(self, request, *args, **kwargs):
+        try:
+            playlist_id = kwargs.get("playlist_id")
+            social_auth = request.user.social_auth.first()
+            sp = spotipy.Spotify(auth=social_auth.access_token)
+            tracks = self.get_tracks(request, playlist_id=playlist_id)
+            track_ids = map(lambda x: x["track"]["id"], tracks)
+            duplicate_track_ids = list(set(filter(lambda x: track_ids.count(x) > 1, track_ids)))
+            sp.user_playlist_remove_all_occurrences_of_tracks(request.user.username, playlist_id, duplicate_track_ids)
+            sp.user_playlist_add_tracks(request.user.username, playlist_id, duplicate_track_ids)
+            return super(DuplicateCleanerView, self).get(request, **kwargs)
+        except spotipy.SpotifyException, e:
+            messages.warning(request, ugettext(u"The Access Token expired."))
+            return redirect("%s?next=/" % reverse("auth:logout"))
+
